91573975ac911f9d3e35ed2a4584b4197e88c4bf
Harry Michael <ando.lalaina1103@gmail.com>
2021-07-09 14:21:26 +0300

    Added tip card/ CSS Refactoring/ Added credentials to README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # w1-visit_card
 Weekly coding challenge for TechJS - Week 1
+
+Full Name : RANDRIAJAOSON Michael
+Faculty : ESIIA-5 
+Number : <NaN>
--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 var card = document.querySelector("div.card");
+var clickTip = document.querySelector("div.tip");
 
 function flipCard() {
     card.classList.toggle('flipped');
+    clearTimeout(showTipProcess);
+}
+
+function showTip() {
+    showTipProcess = setTimeout(handler => {
+        clickTip.classList.toggle('show')
+    }, 2000);
 }
 
 card.addEventListener('click', event => flipCard());
+window.addEventListener('load', event => showTip());
